e005b7ba77a97085ca290142027230f023fb6d2c
Hopeace <1987495374@qq.com>
2022-06-23 09:38:06 +0800

    v0.2.4 简单优化一下getdents函数，测试结果不符合预期

--- 0/linux/fs/readdir.c
+++ 0/linux/fs/readdir.c
@@ -11,7 +11,6 @@ struct linux_dirent
     char d_name[];
 };
 
-
 int getdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count)
 {
     struct linux_dirent lastdirent;
@@ -19,22 +18,25 @@ int getdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count)
     struct buffer_head *path_head = bread(inode->i_dev, inode->i_zone[0]);
     struct dir_entry *path = (struct dir_entry *)path_head->b_data;
     int temp = 0;
-    for (int i = 0; i < 1024; i++)
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    for (i; i < 1024; i++)
     {
-        if (path->inode == 0 || (i+1) * 24 > count)
+        if (path->inode == 0 || (i+1) * 24 > count){
             break;
-            lastdirent.d_ino = path[i].inode;
-            for (int j = 0; j < 14; j++)
-            {
-                lastdirent.d_name[j] = path[i].name[j];
-            }
-            lastdirent.d_off = 0;
-            lastdirent.d_reclen = 24;
-            for (int k = 0; k < 24; k++){
-                put_fs_byte(((char *)&lastdirent)[k], ((char *)dirp + temp));
-                temp++;
-            }
+        }
+        lastdirent.d_ino = path[i].inode;
+        for (j; j < 14; j++)
+        {
+            lastdirent.d_name[j] = path[i].name[j];
+        }
+        lastdirent.d_off = 0;
+        lastdirent.d_reclen = 24;
+        for (k; k < 24; k++){
+            put_fs_byte(((char*)&lastdirent)[k], ((char*)dirp+temp));
+            temp++;
+        }
     }
     return temp;
 }
-
