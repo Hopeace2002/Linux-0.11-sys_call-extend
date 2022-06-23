6bb5e654e172186213c2ad00105874fac538335d
Hopeace <1987495374@qq.com>
2022-06-23 08:49:00 +0800

    v0.2.1 初步测试getdents函数功能

---
+++ 0/linux/fs/readdir.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <linux/fs.h>
+#include <linux/sched.h>
+#include <asm/segment.h>
+// 引入结构体
+struct linux_dirent
+{
+    long d_ino;
+    off_t d_off;
+    unsigned short d_reclen;
+    char d_name[];
+};
+
+
+int getdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count)
+{
+    struct linux_dirent lastdirent;
+    struct m_inode *inode = current->filp[fd]->f_inode;
+    struct buffer_head *path_head = bread(inode->i_dev, inode->i_zone[0]);
+    struct dir_entry *path = (struct dir_entry *)path_head->b_data;
+    int temp = 0;
+    for (int i = 0; i < 1024; i++)
+    {
+        if (path->inode == 0 || (i+1) * 24 > count)
+            break;
+            lastdirent.d_ino = path[i].inode;
+            for (int j = 0; j < 14; j++)
+            {
+                lastdirent.d_name[j] = path[i].name[j];
+            }
+            lastdirent.d_off = 0;
+            lastdirent.d_reclen = 24;
+            for (int k = 0; k < 24; k++){
+                put_fs_byte(((char *)&lastdirent)[k], ((char *)dirp + temp));
+                temp++;
+            }
+    }
+    return temp;
+}
+
